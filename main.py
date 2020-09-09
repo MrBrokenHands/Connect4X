8d4514cf3d5c2b1cc0e9648b50be8f8a67c2c9a1
Tom Comrie <comrietom@gmail.com>
2020-09-09 16:23:56 +0000

    20.9.9 5.23

--- main.py
+++ main.py
@@ -3,31 +3,41 @@ grid = [["-" * 7] * 6]
 
 #Menu
 def menu(): 
-    menu = "Connect 4 - Press the enter key to play"
-    print(menu)
+    print("Connect 4 - Press the enter key to play")
     input()
+    currentPlayer = str(input("Red or Yellow?")) #Either set as "Y" or "R"
 
 def move():
-    while True:
-      while True:
-        try:
-          column = ""
-          column = int(input("Pick a Column (1-7)"))
+  valid = False
+  while True:
+    while valid == False:
+      try:
+        column = ""
+        column = int(input("Pick a Column (1-7)"))
+        valid = True
+      except:
+          print("Please enter a number")
+      try:
+        if column >= 1 and column <= 7:
           break
-        except:
-          print("Please enter a number retard")
-        try:
-          if column >= 1 and column <= 7:
-            print("Yes")
-            break
-          else:
-            print("Input is not valid")
-            continue
-        except:
-            print("not valid input")
-    #if grid
+        else:
+          print("Input is not valid number")
+          valid = False
+          continue
+      except:
+        print("Input is not valid number")
+    break
+  return(column)
 
-    
-menu()
-move()
+def checkMove(column):
+    for x in range(6):
+      print(x)
+      if grid[x, column] != "-":
+          grid[x, column] = 
+
+#menu()
+column=move()
+checkMove(column)
+print("END OF PROGRAM")
+
 #James Worley and Tom Comrie 09/09/2020
