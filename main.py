3290bcaf7729073c04983b8b41a94c1f48eb2fc5
Tom Comrie <comrietom@gmail.com>
2020-09-09 18:48:12 +0000

    20.9.9 7.48

--- main.py
+++ main.py
@@ -89,7 +89,34 @@ def makeMove(column, currentPlayer):
   displayGrid()
 
 def checkWinner():
-  
+  boardWidth = 6
+  boardHeight = 7
+  tile = currentPlayer
+  # check horizontal spaces
+  for y in range(boardHeight):
+      for x in range(boardWidth - 3):
+            if grid[x][y] == tile and grid[x+1][y] == tile and grid[x+2][y] == tile and grid[x+3][y] == tile:
+                return True
+
+    # check vertical spaces
+    for x in range(boardWidth):
+        for y in range(boardHeight - 3):
+            if grid[x][y] == tile and grid[x][y+1] == tile and grid[x][y+2] == tile and grid[x][y+3] == tile:
+                return True
+
+    # check / diagonal spaces
+    for x in range(boardWidth - 3):
+        for y in range(3, boardHeight):
+            if grid[x][y] == tile and grid[x+1][y-1] == tile and grid[x+2][y-2] == tile and grid[x+3][y-3] == tile:
+                return True
+
+    # check \ diagonal spaces
+    for x in range(boardWidth - 3):
+        for y in range(boardHeight - 3):
+            if grid[x][y] == tile and grid[x+1][y+1] == tile and grid[x+2][y+2] == tile and grid[x+3][y+3] == tile:
+                return True
+
+    return False
         
 #Start of Program
 winCondition = False
@@ -98,6 +125,7 @@ while winCondition == False:
   column = move()
   column = column - 1
   makeMove(column, currentPlayer)
+  winCondition = checkWinner()
   if currentPlayer == "R":
     currentPlayer = "Y"
   elif currentPlayer =="Y":
